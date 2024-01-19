f8c97cc12bc926271a452a243bd821ceefb51a7f
LeandroBustos <LeandroBustos@github.com>
2024-01-19 20:50:02 -0300

    fix: youtube video info route

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,10 @@ module.exports = {
     'no-underscore-dangle': 0,
     'no-param-reassign': 0,
     'no-return-assign': 0,
+    'import/no-extraneous-dependencies': 0,
     camelcase: 0,
+    indent: 0,
+    semi: 0,
+    'arrow-parens': 0,
   },
 };
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -3,10 +3,14 @@ const { getVideoDataById } = require('../services/youtube');
 
 const router = express.Router();
 
-router.get('/video/:id/info', (req, res) => {
+router.get('/video/:id/info', async (req, res) => {
     const { id } = req.params
-    const videoInfo = getVideoDataById(id)
-    res.json(videoInfo);
+    try {
+        const videoInfo = await getVideoDataById(id)
+        return res.json(videoInfo);
+    } catch (err) {
+        return res.statusCode(500).end();
+    }
 });
 
 module.exports = router;
