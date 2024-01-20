faf67b55599c7af6418f32c1fa04b064c4dbf7b9
LeandroBustos <LeandroBustos@github.com>
2024-01-19 21:00:51 -0300

    chore: Remove return before next

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -3,13 +3,13 @@ const { getVideoDataById } = require('../services/youtube');
 
 const router = express.Router();
 
-router.get('/video/:id/info', async (req, res) => {
+router.get('/video/:id/info', async (req, res, next) => {
     const { id } = req.params
     try {
         const videoInfo = await getVideoDataById(id)
         return res.json(videoInfo);
     } catch (err) {
-        return res.statusCode(500).end();
+        next()
     }
 });
 
