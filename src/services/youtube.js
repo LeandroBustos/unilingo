bd724ed7e683b370f55066097eee6bb4640253df
LeandroBustos <LeandroBustos@github.com>
2024-01-20 04:25:27 -0300

    fix: Error when video has disabled comments

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -11,23 +11,30 @@ const client = google.youtube({
 const getVideoDataById = async (id) => {
     try {
         const videoInfoResponse = await client.videos.list({
-                part: 'snippet,contentDetails,statistics',
-                id,
-            })
+            part: 'snippet,contentDetails,statistics',
+            id,
+        })
         const videoInfo = videoInfoResponse.data.items[0];
         const title = videoInfo.snippet.title;
         const viewCount = videoInfo.statistics.viewCount;
         if (!videoInfo) throw new Error('Video Not Found')
-        const commentsInfoResponse = await client.commentThreads.list({
+
+        let latestCommentText = null
+        try {
+            const commentsInfoResponse = await client.commentThreads.list({
                 part: 'snippet',
                 videoId: id,
                 order: 'time', // Order comments by time to get the latest
                 maxResults: 1, // Get only the latest comment
             });
-        if (!commentsInfoResponse) throw new Error('Comments Not Found')
+            if (commentsInfoResponse) {
+                const latestComment = commentsInfoResponse.data.items[0];
+                latestCommentText = latestComment.snippet.topLevelComment.snippet.textDisplay;
+            }
+        } catch (err) {
+            console.error('Error al obtener la información de los comentarios del video:', err.message);
+        }
 
-        const latestComment = commentsInfoResponse.data.items[0];
-        const latestCommentText = latestComment.snippet.topLevelComment.snippet.textDisplay;
         return {
             id,
             title,
